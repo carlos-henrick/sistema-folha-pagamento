a8d09eb9fd02743ea2aff1d1b8d0f3c0eb632e40
Carlos <141372667+carlos-henrick@users.noreply.github.com>
2026-06-01 21:11:19 -0300

    registro de Func comissionado
    criado o registro de Func comissionado

--- main.java
+++ main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
-public class Aprender {
-    static int saida, menu, i, x;
-    static String[][] funcionarios = new String[99][99]; 
-    static int indice;
+public class Main {
+
+    //Variáveis
+    public static final String salario = "2000";
+    public static float x;
+    public static int saida, menu, i;
+    public static String[][] funcionarios = new String[99][99]; 
+    public static int indice = 0;
 
     public static int VerificarEspaco(String[][] funcionarios){
         for (i = 0; i < funcionarios.length; i++ ) {
@@ -23,8 +27,9 @@ public class Aprender {
 
     public static void main(String[] args) {
         Scanner Input = new Scanner(System.in);
+
+        
         saida = 1;
-        indice = 0;
         
         while (saida == 1){
             menu();
@@ -48,9 +53,10 @@ public class Aprender {
                             while (funcionarios[indice][1].length() != 4 || !funcionarios[indice][1].matches("\\d+")){
                                 System.out.println("A matrícula deve conter exatamente 4 dígitos.");
                                 System.out.println("Seja apenas números ");
-                                System.out.print("Matricula: ");
+                                System.out.print("Matrícula: ");
                                 funcionarios[indice][1] = Input.nextLine();
                             }
+                            funcionarios[indice][2] = salario;
                             System.out.println("Deseja fazer mais um cadastro?");
                             System.out.println("(1)Sim");
                             System.out.println("(2)Não");
@@ -67,7 +73,71 @@ public class Aprender {
                             }
                     }
                     break;
-            
+                case 2:
+                    System.out.println("Cadastro de Funcionários Comissionado\n");
+                    indice = VerificarEspaco(funcionarios);
+                    while (indice <= funcionarios.length){
+                        System.out.print("Nome: ");
+                        funcionarios[indice][0] = Input.nextLine();
+                        
+                        while (funcionarios[indice][0].length() < 4){
+                            System.out.println("Mínimo de caracteres é 4");
+                            System.out.print("Nome: ");
+                            funcionarios[indice][0] = Input.nextLine();
+                        }
+                        System.out.print("Matrícula: ");
+                        funcionarios[indice][1] = Input.nextLine();
+                        while (funcionarios[indice][1].length() != 4 || !funcionarios[indice][1].matches("\\d+")){
+                            System.out.println("A matrícula deve conter exatamente 4 dígitos.");
+                            System.out.println("Seja apenas números");
+                            System.out.print("Matrícula: ");
+                            funcionarios[indice][1] = Input.nextLine();
+                        }
+                        funcionarios[indice][2] = salario;
+
+                        System.out.print("Informe o valor das vendas: ");
+                        funcionarios[indice][3] = Input.nextLine(); 
+                        x = Float.parseFloat(funcionarios[indice][3]);                  
+                        while (x < 0 || !funcionarios[indice][3].matches("\\d+")){
+                            System.out.println("Informe um valor válido");
+                            System.out.println("Seja apenas números(ex: 203.00)");
+                            System.out.print("Informe o valor das vendas: ");
+                            funcionarios[indice][3] = Input.nextLine(); 
+                            x = Float.parseFloat(funcionarios[indice][3]);
+                        }
+
+                        System.out.print("Informe comissão percentual: ");
+                        funcionarios[indice][4] = Input.nextLine();
+                        while (!funcionarios[indice][4].matches("\\d+")){
+                            System.out.println("Informe um valor válido");
+                            System.out.println("Seja apenas números");
+                            System.out.print("Informe comissão percentual: ");
+                            funcionarios[indice][4] = Input.nextLine();
+                        }
+                        System.out.println("Deseja fazer mais um cadastro?");
+                        System.out.println("(1)Sim");
+                        System.out.println("(2)Não");
+                        System.out.print("->");
+                        menu = Input.nextInt();
+                        Input.nextLine();
+                        if (menu == 2){
+                            break;
+                        }
+                        indice = VerificarEspaco(funcionarios);
+                        if (indice == 100){
+                            System.out.println("Limite de cadastros atingido!");
+                            break;
+                        }
+
+                    }
+                    break;
+                case 3:
+                    System.out.println("Cadastrar Funcionário Produção\n");
+                    indice = VerificarEspaco(funcionarios);
+                    while (indice <= funcionarios.length){
+                        System.out.println("Nome: ");
+                        funcionarios[indice][0] = Input.nextLine();
+                    }
                 default:
                     
                     break;
